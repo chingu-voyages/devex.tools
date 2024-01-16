35aef5be44fe7cfdb609023daba447f7b1523867
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2024-01-15 20:59:58 -0500

    Declares variables

--- fetools-app/src/pages/CharacterFinder.jsx
+++ fetools-app/src/pages/CharacterFinder.jsx
@@ -1,10 +1,18 @@
 import '../index.css';
+import React, {useState} from 'react';
+import he from 'he';
+
 
 function CharacterFinder() {
+
+  const [inputValue, setInputValue] = useState("");
+  const [encodedValue, setEncodedvalue] = useState("");
     return (
       <div>
         <p className="font-mono text-6xl">Character Finder</p>
       </div>
+
+      
     )
 }
   
