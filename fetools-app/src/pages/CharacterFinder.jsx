9f3daf4cc10e0f71c42f8ac55210b8433500491e
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2024-01-15 21:16:51 -0500

    Adds input text field

--- fetools-app/src/pages/CharacterFinder.jsx
+++ fetools-app/src/pages/CharacterFinder.jsx
@@ -8,9 +8,14 @@ function CharacterFinder() {
   const [inputValue, setInputValue] = useState("");
   const [encodedValue, setEncodedvalue] = useState("");
     return (
-      <div>
+      <><div>
         <p className="font-mono text-6xl">Character Finder</p>
-      </div>
+      </div><div>
+          <input type="text" value={inputValue} />
+          <button onClick={""}>Encode</button>
+
+
+        </div></>
 
       
     )
