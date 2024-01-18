cd2c05cd0b5df51ccbba62bde0f89b1517f2248d
Aman Gill <gillaman1998@gmail.com>
2024-01-18 14:13:14 -0500

    Revert "Make base size input box only editable when clicking the gear icon, and add comments to make code more readable"
    This reverts commit 520e1058e0739bd231d873edf5cc67b6d7a42b61.

--- fetools-app/src/App.jsx
+++ fetools-app/src/App.jsx
@@ -1,38 +1,33 @@
-import {
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import UnitConverter from "./pages/UnitConverter";
-import ColorPicker from "./pages/ColorPicker";
-import CharacterFinder from "./pages/CharacterFinder";
-import FontVisualizer from "./pages/FontVisualizer";
-import Team from "./pages/Team";
-import ErrorPage from "./pages/ErrorPage";
-import "./index.css";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+import UnitConverter from './pages/UnitConverter';
+import ColorPicker from './pages/ColorPicker';
+import CharacterFinder from './pages/CharacterFinder';
+import FontVisualizer from './pages/FontVisualizer'
+import Team from './pages/Team'
+import ErrorPage from './pages/ErrorPage';
+import './index.css'
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Header />} errorElement={<ErrorPage />}>
-      <Route index element={<Home />} />
-      <Route path="unit-converter" element={<UnitConverter />} />
-      <Route path="color-picker" element={<ColorPicker />} />
-      <Route path="character-finder" element={<CharacterFinder />} />
-      <Route path="font-visualizer" element={<FontVisualizer />} />
-      <Route path="team" element={<Team />} />
-    </Route>
-  )
-);
+    createRoutesFromElements(
+      <Route path="/" element={<Header/>} errorElement={<ErrorPage/>}>
+        <Route index element={<Home/>} />
+        <Route path="unit-converter" element={<UnitConverter/>} />
+        <Route path="color-picker" element={<ColorPicker/>} />
+        <Route path="character-finder" element={<CharacterFinder/>} />
+        <Route path="font-visualizer" element={<FontVisualizer/>} />
+        <Route path="team" element={<Team/>} />
+      </Route>
+    )
+)
 
-function App({ routes }) {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+function App({routes}){
+    return (
+        <>
+          <RouterProvider router={router}/>
+        </>
+      );
 }
 
-export default App;
+export default App
