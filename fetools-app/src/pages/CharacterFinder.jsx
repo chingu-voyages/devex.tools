8e791f140ae11369fae1c4cd8eb2b32d9ffff6ad
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2024-01-19 23:35:10 -0500

    "Resolves merge conflict"

---
+++ fetools-app/src/pages/CharacterFinder.jsx
@@ -0,0 +1,11 @@
+import '../index.css';
+
+function CharacterFinder() {
+    return (
+      <main>
+        <p className="font-mono text-6xl">Character Finder Page</p>
+      </main>
+    )
+}
+  
+export default CharacterFinder
