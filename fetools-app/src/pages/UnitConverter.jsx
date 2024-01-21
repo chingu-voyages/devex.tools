5a6f298f83beb391403b9380d148201acddc66d0
Aman Gill <gillaman1998@gmail.com>
2024-01-21 00:52:16 -0500

    feat: Create Lorem Ipsum preview that changes font size as per user's input

--- fetools-app/src/pages/UnitConverter.jsx
+++ fetools-app/src/pages/UnitConverter.jsx
@@ -1,4 +1,5 @@
 import "../index.css";
+import GoDeeper from "../components/GoDeeper";
 
 import React, { useState } from "react";
 
@@ -11,6 +12,12 @@ function UnitConverter() {
   const [em, setEm] = useState(0);
   const [tailwindSize, setTailwindSize] = useState(0);
   const [editMode, setEditMode] = useState(false);
+  const [cssSize, setCssSize] = useState("16px");
+
+  // Update CSS size whenever pixels, em, or Tailwind size changes
+  const updateCssSize = (newSizeInPixels) => {
+    setCssSize(`${newSizeInPixels}px`); // Update CSS size
+  };
 
   // Handler for base pixel size changes
   const handleBasePixelSizeChange = (e) => {
@@ -28,6 +35,7 @@ function UnitConverter() {
     const newEm = newPixels / basePixelSize;
     setEm(newEm);
     setTailwindSize(newEm * 4);
+    updateCssSize(newPixels);
   };
 
   // Handler for rem/em value changes
@@ -37,6 +45,7 @@ function UnitConverter() {
     const newPixels = newEm * basePixelSize;
     setPixels(newPixels);
     setTailwindSize(newEm * 4);
+    updateCssSize(newPixels);
   };
 
   // Handler for Tailwind size changes
@@ -46,6 +55,7 @@ function UnitConverter() {
     const newEm = newTailwindSize / 4;
     setEm(newEm);
     setPixels(newEm * basePixelSize);
+    updateCssSize(newPixels);
   };
 
   // Handler for the cog (settings) icon click. Toggles the edit mode state.
@@ -61,80 +71,123 @@ function UnitConverter() {
     }
   };
 
+  //links for Go Deeper component
+  const linksData = [
+    {
+      url: "https://en.wikipedia.org/wiki/Gabe_Newell",
+      textValue: "Dummy Link 1",
+    },
+    {
+      url: "https://en.wikipedia.org/wiki/SteamOS",
+      textValue: "Dummy Link 2",
+    },
+    {
+      url: "https://en.wikipedia.org/wiki/Half-Life:_Alyx",
+      textValue: "Dummy Link 3",
+    },
+  ];
+
   // JSX for rendering the UI components.
   return (
-    <main>
-      <p className="font-mono text-5xl ml-3">Unit Converter</p>
-      <p className="font-mono text-1xl ml-3 mb-2">
-        Calculate PX, REM/EM, and Tailwind utility classes with ease.
-      </p>
-      <div className="flex gap-10 p-4">
-        <div className="mb-3">
-          <label className="block mb-2 text-sm font-bold text-black">
-            Base Size
-          </label>
-          {/*Tertiary operator used to differentiate when the Base Size input element should and should not be editable*/}
-          {editMode ? (
-            <input
-              type="number"
-              className="border rounded w-28 py-2 px-3  text-gray-700 leading-tight"
-              value={basePixelSize}
-              onChange={handleBasePixelSizeChange}
-              onBlur={handleBaseSizeInputBlur}
-            />
-          ) : (
-            <div className="flex items-center">
-              <span className="mr-2">{basePixelSize}</span>
-              <button onClick={handleCogClick}>⚙️</button>
-            </div>
-          )}
-        </div>
-        <div className="mb-3 relative">
-          <label className="block mb-2 text-sm font-bold text-black">
-            REM/EM
-          </label>
-          <div className="flex border rounded relative">
-            <input
-              type="number"
-              className="border rounded w-28 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={em}
-              onChange={handleEmChange}
-            />
-            <span className="px-2 text-gray-700 absolute inset-y-0 right-0 flex items-center mr-2 pointer-events-none">
-              rem
-            </span>
+    <>
+      <main
+        className="p-6
+    sm:p-12 lg:px-48 lg:py-20"
+      >
+        <p className="font-mono text-6xl ml-4">Unit Converter</p>
+
+        <p className="font-mono text-1xl ml-4 mb-2">
+          Calculate PX, REM/EM, and Tailwind utility classes with ease.
+        </p>
+
+        <div className="flex gap-10 p-4 ml-52">
+          <div className="mb-3">
+            <label className="block mb-2 text-lg font-bold text-black">
+              Base Size
+            </label>
+            {/*Tertiary operator used to differentiate when the Base Size input element should and should not be editable*/}
+            {editMode ? (
+              <input
+                type="number"
+                className="border rounded w-28 py-2 px-3  text-gray-700 leading-tight"
+                value={basePixelSize}
+                onChange={handleBasePixelSizeChange}
+                onBlur={handleBaseSizeInputBlur}
+              />
+            ) : (
+              <div className="flex items-center">
+                <span className="mr-2">{basePixelSize}</span>
+                <button onClick={handleCogClick}>⚙️</button>
+              </div>
+            )}
           </div>
-        </div>
-        <div className="mb-3 relative">
-          <label className="block mb-2 text-sm font-bold text-black">
-            Pixels
-          </label>
-          <div className="flex border rounded relative">
+
+          <div className="mb-3 relative">
+            <label className="block mb-2 text-lg font-bold text-black">
+              REM/EM
+            </label>
+            <div className="flex border rounded relative">
+              <input
+                type="number"
+                className="border rounded w-28 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={em}
+                onChange={handleEmChange}
+              />
+              <span className="px-2 text-gray-700 absolute inset-y-0 right-0 flex items-center mr-2 pointer-events-none">
+                rem
+              </span>
+            </div>
+          </div>
+
+          <div className="mb-3 relative">
+            <label className="block mb-2 text-lg font-bold text-black">
+              Pixels
+            </label>
+            <div className="flex border rounded relative">
+              <input
+                type="number"
+                className="border rounded w-28 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                value={pixels}
+                onChange={handlePixelChange}
+              />
+              <span className="px-2 text-gray-700 absolute inset-y-0 right-0 flex items-center mr-2 pointer-events-none">
+                px
+              </span>
+            </div>
+          </div>
+
+          <div className="mb-3">
+            <label className="block mb-2 text-lg font-bold text-black">
+              Tailwind Size
+            </label>
             <input
               type="number"
-              className="border rounded w-28 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={pixels}
-              onChange={handlePixelChange}
+              className="border rounded w-28 py-2 px-3 text-gray-700 leading-tight"
+              value={tailwindSize}
+              onChange={handleTailwindChange}
             />
-            <span className="px-2 text-gray-700 absolute inset-y-0 right-0 flex items-center mr-2 pointer-events-none">
-              px
-            </span>
           </div>
         </div>
 
-        <div className="mb-3">
-          <label className="block mb-2 text-sm font-bold text-black">
-            Tailwind Size
-          </label>
-          <input
-            type="number"
-            className="border rounded w-28 py-2 px-3 text-gray-700 leading-tight"
-            value={tailwindSize}
-            onChange={handleTailwindChange}
-          />
+        <div className="flex flex-col gap-4 items-start p-4">
+          <p className="font-mono text-4xl">Preview</p>
+          <div
+            className="flex flex-row justify-start items-center border border-black border-dashed p-3 
+        min-h-[100px] w-full overflow-auto"
+          >
+            <p
+              className="font-mono font-bold text-3xl break-words leading-none"
+              style={{
+                fontSize: cssSize,
+              }}
+            >
+              Lorem ipsum dolor sit amet
+            </p>
+          </div>
         </div>
-      </div>
-    </main>
+        {/* <GoDeeper linksData={linksData} /> */}
+      </main>
+    </>
   );
 }
 
