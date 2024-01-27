a0b19be2a68b319e7cf2c1001934d9e6afff3d97
Aman Gill <gillaman1998@gmail.com>
2024-01-27 03:58:29 -0500

    Add copy button to code block component

--- fetools-app/src/components/CodeBlock.jsx
+++ fetools-app/src/components/CodeBlock.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import CopyButton from "./CopyButton";
 
 function CodeBlock({ title, code }) {
+  // Function to provide the code text to the CopyButton
+
+  const handleCopy = () => code;
+
   return (
-    <div className="mb-4">
+    <div className="mb-4 relative">
       <div className="mb-2 text-sm font-bold text-gray-400">{title}</div>
-      <div className="p-3 bg-gray-200 rounded">
+      <div className="absolute top-0 right-0 mb-2">
+        <CopyButton onCopy={handleCopy} />
+      </div>
+      <div className="p-3 bg-gray-200 rounded relative">
         <code>{code}</code>
       </div>
     </div>
