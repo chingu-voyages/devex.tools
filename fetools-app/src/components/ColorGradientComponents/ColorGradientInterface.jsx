b860d00243bbd6d39475567d1f868018b7e8d22f
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2024-02-01 05:40:25 -0600

    added html5 picker

--- fetools-app/src/components/ColorGradientComponents/ColorGradientInterface.jsx
+++ fetools-app/src/components/ColorGradientComponents/ColorGradientInterface.jsx
@@ -28,7 +28,8 @@ export default function ColorGradientInterface({
         if(parentRef.current){
             [...parentRef.current.children].forEach(label=>{
                 if(label.id === 'color'){
-                    label.children[0].value = inputValue.color
+                    label.children[0].children[0].value = inputValue.color
+                    label.children[0].children[1].value = inputValue.color
                 } else if(label.id === 'position'){
                     label.children[0].value = inputValue.position
                 } else if(label.id === 'rotation'){
@@ -41,15 +42,23 @@ export default function ColorGradientInterface({
     return(
         <>
         <div ref={parentRef} className="grid grid-cols-2 grid-rows-3 gap-x-7 gap-y-12 px-5">
-            <label id="color" className="flex flex-col w-full font-bold ">Color
-                <input 
-                ref={colorInputRef} 
-                defaultValue={displayData.color}
-                onChange={handleColorInputChange} 
-                placeholder={lastValidData.color || displayData.color}
-                type="text" 
-                maxLength={7}
-                className="rounded-sm border border-gray-400 p-4 uppercase"/>
+            <label id="color" className="relative flex flex-col w-full font-bold ">Color
+                <div className="relative w-full z-0">
+                    <input 
+                    ref={colorInputRef} 
+                    defaultValue={displayData.color}
+                    onChange={handleColorInputChange} 
+                    placeholder={lastValidData.color || displayData.color}
+                    type="text" 
+                    maxLength={7}
+                    className="rounded-sm border border-gray-400 p-4 uppercase flex-2"/>
+
+                    <input 
+                    type="color" 
+                    defaultValue={displayData.color} 
+                    onChange={handleColorInputChange}
+                    className="absolute right-0 top-4"/>
+                </div>
             </label>
             <label id="position" className="flex flex-col w-full font-bold">Position
                 <input
