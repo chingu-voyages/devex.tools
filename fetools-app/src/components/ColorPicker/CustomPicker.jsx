cb41fe7dd1b9be494756c4708ff40226ed4ed565
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2024-02-01 21:04:27 -0600

    update custom picker

--- fetools-app/src/components/ColorPicker/CustomPicker.jsx
+++ fetools-app/src/components/ColorPicker/CustomPicker.jsx
@@ -1,23 +1,12 @@
 import { useEffect } from "react"
-import { ChromePicker } from "react-color";
-import reactCSS from "reactcss";
+
+import tinycolor from "tinycolor2";
+
 
 export default function CustomPicker({
   color,
   handleChange
 }){
-
-  const pickerStyle = reactCSS({
-    'default':{
-      picker:{
-        width: '400px',
-        background: '#000',
-      },
-      active:{
-        background: 'none',
-      },
-    }
-  })
 
   useEffect(()=>{
    // renderPicker()
@@ -27,12 +16,29 @@ export default function CustomPicker({
 
   return(
     <>
-      <ChromePicker 
-      className="flex-1 w-100 shadow-none"
-      color={color}
-      onChange={handleChange}
-      styles={pickerStyle}/>
+      <canvas id="color-picker"></canvas>
     </>
   )
+
+    function createGradient(){
+      const colorCanvas = document.getElementById('color_canvas');
+      const ColorCtx = colorCanvas .getContext('2d');  // This create a 2D context for the canvas
+
+      const color = 'rgba(0,0,255,1)';
+      let gradientH = ColorCtx .createLinearGradient(0, 0, ColorCtx .canvas.width, 0);
+      gradientH.addColorStop(0, '#fff');
+      gradientH.addColorStop(1, color);
+      ColorCtx .fillStyle = gradientH;
+      ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, ColorCtx .canvas.height);
+
+
+      // Create a Vertical Gradient(white to black)
+      let gradientV = ColorCtx .createLinearGradient(0, 0, 0, 300);
+      gradientV.addColorStop(0, 'rgba(0,0,0,0)');
+      gradientV.addColorStop(1, '#000');
+      ColorCtx .fillStyle = gradientV;
+      ColorCtx .fillRect(0, 0, ColorCtx .canvas.width, 
+      ColorCtx .canvas.height); 
+    }
 
 }
