4993d94c4df61af35183488452f50bb2bb38d686
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2024-02-01 21:25:23 -0600

    Updating tab switcher style and functionality, and toolheading style

--- fetools-app/src/components/ToolsLayout/ToolHeading.jsx
+++ fetools-app/src/components/ToolsLayout/ToolHeading.jsx
@@ -2,8 +2,9 @@ export default function ToolHeading({title="Dummy Title", tagline="Dummy Tagline
 
     return(
         <>
-            <div className="flex-1 max-sm:max-w-80">
-                <h2 className="block font-bold text-5xl">{title}</h2>
+            <div className="max-w-[26rem] flex-1 
+            max-sm:w-full">
+                <h2 className="block font-bold text-5xl text-nowrap max-sm:text-4xl">{title}</h2>
                 <p className="block text-sm text-gray-400">{tagline}</p>
             </div>
         </>
