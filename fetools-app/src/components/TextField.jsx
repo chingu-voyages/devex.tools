4afae0ce6dea0f971a8fb50ddb3a565a795778ce
joekotvas <joseph.kotvas@gmail.com>
2024-02-03 12:31:57 -0500

    Revert "Merge pull request #43 from chingu-voyages/steven-branch-sprint3v3"
    This reverts commit 689ef7183f919bb8ee3d9b1a24033844d5cfdfd1, reversing
    changes made to 30b86f3908fcd55b8fc15683b655a9e32ef75107.

--- fetools-app/src/components/TextField.jsx
+++ fetools-app/src/components/TextField.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import CopyButton from "./CopyButton";
 
-function TextField({ title, value, unit, onValueChange }) {
+function TextField({
+  title,
+  value,
+  unit,
+  onValueChange,
+  inputType = "number",
+}) {
+  // A ref to the input element
+  const inputRef = useRef(null);
+
   // Function to concatenate value and unit, used for the copy function
   const getValueWithUnit = () => {
     return unit ? `${value} ${unit}` : value.toString();
+  };
+
+  // Function to handle click inside the input field
+  const handleInputClick = () => {
+    if (inputRef.current) {
+      inputRef.current.select(); // Select all text in the input field
+    }
   };
 
   return (
@@ -14,10 +30,12 @@ function TextField({ title, value, unit, onValueChange }) {
       </label>
       <div className="flex border rounded relative">
         <input
-          type="number"
+          ref={inputRef}
+          type={inputType}
           className="border rounded border-black w-28 py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
           value={value}
           onChange={onValueChange}
+          onFocus={handleInputClick}
         />
         {unit && (
           <span className="px-2 text-gray-700 absolute inset-y-0 right-0 flex items-center mr-2 pointer-events-none font-semibold">
