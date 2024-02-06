1559c268ca3e89c40dddb1bdbed54d6a597c5f41
Aman Gill <gillaman1998@gmail.com>
2024-02-06 01:01:19 -0500

    Fix: Number input fields should not display native number controls in browser

--- fetools-app/src/components/TextField.jsx
+++ fetools-app/src/components/TextField.jsx
@@ -25,30 +25,44 @@ function TextField({
   };
 
   return (
-    <div className="mb-3 relative">
-      <label className="block mb-2 text-sm font-bold text-gray-400">
-        {title}
-      </label>
-      <div className="flex border rounded relative">
-        <input
-          ref={inputRef}
-          type={inputType}
-          className="border rounded border-black w-28 py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
-          value={value}
-          onChange={onValueChange}
-          onFocus={handleInputClick}
-          onBlur={onBlur}
-        />
-        {unit && (
-          <span className="px-2 text-gray-700 absolute inset-y-0 right-0 flex items-center mr-2 pointer-events-none font-semibold">
-            {unit}
+    <>
+      <style>
+        {`
+          input[type='number']::-webkit-inner-spin-button,
+          input[type='number']::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+          }
+          input[type='number'] {
+            -moz-appearance: textfield;
+          }
+        `}
+      </style>
+      <div className="mb-3 relative">
+        <label className="block mb-2 text-sm font-bold text-gray-400">
+          {title}
+        </label>
+        <div className="flex border rounded relative">
+          <input
+            ref={inputRef}
+            type={inputType}
+            className="border rounded border-black w-28 py-2 px-3 text-gray-400 leading-tight focus:outline-none focus:shadow-outline"
+            value={value}
+            onChange={onValueChange}
+            onFocus={handleInputClick}
+            onBlur={onBlur}
+          />
+          {unit && (
+            <span className="px-2 text-gray-700 absolute inset-y-0 right-0 flex items-center mr-2 pointer-events-none font-semibold">
+              {unit}
+            </span>
+          )}
+          <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-full text-lg -mt-2">
+            <CopyButton onCopy={getValueWithUnit} />
           </span>
-        )}
-        <span className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-full text-lg -mt-2">
-          <CopyButton onCopy={getValueWithUnit} />
-        </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
