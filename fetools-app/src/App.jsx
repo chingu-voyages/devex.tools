4e53d170059bae07a9b633155e87f4b62df4ab3d
joekotvas <joseph.kotvas@gmail.com>
2024-02-06 18:41:10 -0500

    Initial work on colors, typography & PageSection component

--- fetools-app/src/App.jsx
+++ fetools-app/src/App.jsx
@@ -1,18 +1,19 @@
 import { Outlet } from 'react-router-dom';
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from '@/components/ui/toaster';
 import Header from './components/PageLayout/Header';
 import Footer from './components/PageLayout/Footer';
 
-import './index.css'
+import './index.css';
 
-function App(){
-  return(
-  <>
-    <Header/>
-    <Outlet/>
-    <Toaster/>
-    <Footer/>
-  </>);
+function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Toaster />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
