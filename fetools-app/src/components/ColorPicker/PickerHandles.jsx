1c45b0b22f017645918d157b2d01dd379080233f
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2024-02-06 20:41:51 -0600

    Created saturation slider

--- fetools-app/src/components/ColorPicker/PickerHandles.jsx
+++ fetools-app/src/components/ColorPicker/PickerHandles.jsx
@@ -19,14 +19,23 @@ export default function PickerHandles({
     <>
       <div id="picker-handles-container" className="px-6">
         <ul className="flex flex-1 flex-col">
-          <li className="colorPickerSlider flex gap-x-3 justify-between">
-            <span className="block font-bold text-sm">H</span>
-            <span className="block font-medium text-sm text-gray-400 w-7">{parseInt(sliderValues.hue.h)}°</span>
+          <li className="colorPickerSliderVar flex gap-x-3 justify-between">
+            <span className="block font-bold text-sm w-[10px]">H</span>
+            <span className="block font-medium text-sm text-gray-400 w-9">{parseInt(sliderValues.hue.h)}°</span>
             <input id="hue" max={360} min={0} step={1}
             type="range"
             defaultValue={parseInt(sliderValues.hue.h)}
             onChange={(e)=>handleHueOnChange(e, 'hue')}
-            className="hueSlider flex-1"></input>
+            className="colorPickerSlider hueSlider flex-1 lg:w-[412px]"></input>
+          </li>
+          <li className="colorPickerSliderVar flex gap-x-3 justify-evenly">
+            <span className="block font-bold text-sm w-[10px]">S</span>
+            <span className="block font-medium text-sm text-gray-400 w-9">100%</span>
+            <input id="saturation" max={100} min={0} step={1}
+            type="range"
+            defaultValue={parseInt(sliderValues.hue.h)}
+            onChange={(e)=>handleHueOnChange(e, 'saturation')}
+            className="colorPickerSlider satSlider flex-1"></input>
           </li>
         </ul>
       </div>
