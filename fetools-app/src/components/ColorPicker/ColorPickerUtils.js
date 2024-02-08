1756b9536aa738a61d496ca6afb6019db08b3627
Stevensauro <31481885+Stevensauro@users.noreply.github.com>
2024-02-07 19:52:11 -0600

    fix alpha affecting hue gradient and thumb colors

--- fetools-app/src/components/ColorPicker/ColorPickerUtils.js
+++ fetools-app/src/components/ColorPicker/ColorPickerUtils.js
@@ -18,6 +18,8 @@ export function createColorObj(newColor, newHue){
       alpha: tinycolor(color).getAlpha()
     }
 
+    colorObj.color.a = 1
+
     if(newHue){
       updateHueInColor()
     }
@@ -28,6 +30,7 @@ export function createColorObj(newColor, newHue){
       const hueHsl = {...color}
       hueHsl.s = 1
       hueHsl.l = 0.5
+      hueHsl.a = 1
       return hueHsl
     }
 
