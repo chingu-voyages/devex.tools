1a98cb7a1e66d9ba0d80c331fd0e3d1227db3d2f
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2024-02-12 09:46:53 -0500

    Updates UI, adds pagination using react-paginate, updates search feature

--- fetools-app/src/components/CharacterFinderComponents/CharacterCard.jsx
+++ fetools-app/src/components/CharacterFinderComponents/CharacterCard.jsx
@@ -9,21 +9,44 @@ const CharacterCard = ({
   cssCode,
 }) => {
   return (
-    <div className="p-2 bg-gray-100 rounded-md shadow-md m-4 h-7/8">
-      <p className="text-6xl font-bold text-left">{char}</p>
-      <p className="pt-2 text-xs text-left font-bold uppercase">{name}</p>
-      <p className="pt-2 text-xs text-left uppercase">
-        unicode: <span className="font-bold">{unicode}</span>
+    <div className="p-6 bg-white h-7/8 w-50px">
+      <p className="text-6xl font-bold border-b-4 border-black text-left py-4">
+        {char}
       </p>
-      <p className="pt-2 text-xs text-left uppercase">
-        html code: <span className="font-bold">{htmlcode}</span>
-      </p>
-      <p className="pt-2 text-xs text-left uppercase">
-        html entity: <span className="font-bold">{htmlEntity}</span>
-      </p>
-      <p className="pt-2 text-xs text-left uppercase">
-        css code: <span className="font-bold">{cssCode}</span>
-      </p>
+      <div className="flex flex-wrap">
+        <div className="w-full">
+          <p className="pt-2 text-s text-left font-bold uppercase">{name}</p>
+        </div>
+        <div className="flex flex-wrap">
+          <div className="w-full lg:w-1/2">
+            <p className="pt-2 text-xs text-left uppercase">
+              unicode: <p><span className="font-bold">{unicode || " -- "}</span></p>
+            </p>
+          </div>
+          
+          <div className="w-full lg:w-1/2">
+            <p className="pt-2 text-xs text-left uppercase">
+              html code: <p><span className="font-bold flex flex-wrap">{htmlcode || " -- "}</span></p>
+            </p>
+            </div>
+         
+          <div className="w-full lg:w-1/2">
+            <p className="pt-2 text-xs text-left uppercase">
+              html entity:
+              <p><span className="font-bold">
+                {htmlEntity !== undefined && htmlEntity !== ""
+                  ? htmlEntity
+                  : " -- "}
+              </span></p>
+            </p>
+          </div>
+          <div className="w-full lg:w-1/2">
+            <p className="pt-2 text-xs text-left uppercase">
+              css code: <p><span className="font-bold">{cssCode || " -- "}</span></p>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
