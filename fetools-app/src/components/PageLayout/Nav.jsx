284e094110cbb896357dae976eeffaef52017c45
joekotvas <joseph.kotvas@gmail.com>
2024-02-16 09:21:06 -0500

    Create icon component and add icons to header

--- fetools-app/src/components/PageLayout/Nav.jsx
+++ fetools-app/src/components/PageLayout/Nav.jsx
@@ -1,28 +1,56 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Icon from '../Icon';
+
+const Links = [
+  { to: '/characters', icon: 'star', text: 'Characters' },
+  { to: '/colors', icon: 'colorize', text: 'Colors' },
+  { to: '/fonts', icon: 'format_size', text: 'Fonts' },
+  { to: '/gradients', icon: 'gradient', text: 'Gradients' },
+  { to: '/shadows', icon: 'ev_shadow', text: 'Shadows' },
+  { to: '/units', icon: 'sync', text: 'Units', className: 'rotate-90' },
+].map(({ to, icon, text, className }) => (
+  <li key={to} className="border-gray-200 max-md:w-full max-md:border">
+    <Link to={to} className="max-md:p-4">
+      <Icon name={icon} className={className && className} /> {text}
+    </Link>
+  </li>
+));
 
 export default function Nav() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  useEffect(() => {
+    const clickListener = e =>
+      e.target.closest('.hamburger') === null && setIsExpanded(false);
+
+    const escListener = e => e.key === 'Escape' && setIsExpanded(false);
+
+    document.addEventListener('click', clickListener);
+    document.addEventListener('keydown', escListener);
+    return () => {
+      document.removeEventListener('click', clickListener);
+      document.removeEventListener('keydown', escListener);
+    };
+  }, []);
+
   return (
-    <nav className="max-sm:hidden flex w-full justify-center lg:justify-end">
-      <ul className="flex justify-center lg:justify-end font-bold gap-12 h-full items-center">
-        <li>
-          <Link to="/characters">Characters</Link>
-        </li>
-        <li>
-          <Link to="/colors">Colors</Link>
-        </li>
-        <li>
-          <Link to="/fonts">Fonts</Link>
-        </li>
-        <li>
-          <Link to="/gradients">Gradients</Link>
-        </li>
-        <li>
-          <Link to="/shadows">Shadows</Link>
-        </li>
-        <li>
-          <Link to="/units">Units</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav
+        className={`flex w-full justify-center gap-0 bg-white max-md:absolute max-md:right-0 max-md:top-20 max-md:z-50 md:pb-4 xl:pb-0 xl:justify-end max-md:${
+          isExpanded ? '' : 'hidden'
+        }`}
+      >
+        <ul className="max-md:border-1 max-md:w-screen flex h-full flex-wrap items-center justify-center gap-x-4 font-bold max-md:flex-col max-md:border-gray-200 max-md:text-center max-md:shadow-md sm:w-full sm:justify-center lg:gap-8 xl:justify-end [&>li>a]:flex [&>li>a]:gap-2 [&_.icon]:text-accent">
+          {Links}
+        </ul>
+      </nav>
+      <Icon
+        name={isExpanded ? `close` : `menu`}
+        size="40"
+        className="hamburger cursor-pointer select-none md:hidden"
+        onClick={() => setIsExpanded(prev => !prev)}
+      />
+    </div>
   );
 }
