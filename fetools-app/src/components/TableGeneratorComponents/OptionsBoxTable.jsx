d82b97891c0779fa481f4b42a9e6d21961009106
joekotvas <joseph.kotvas@gmail.com>
2024-02-18 05:36:04 -0500

    Update table generator with global layout components

--- fetools-app/src/components/TableGeneratorComponents/OptionsBoxTable.jsx
+++ fetools-app/src/components/TableGeneratorComponents/OptionsBoxTable.jsx
@@ -1,56 +1,22 @@
-import optionMenu from "../../assets/tune.svg";
-import addbookmark from "../../assets/bookmark_add.svg";
-import share from "../../assets/share.svg";
-import rowsAndCols from "../../assets/table.svg";
-import paddingIcons from "../../assets/padding.svg";
-import doubleArrows from "../../assets/doubleArrow.svg";
-import alingLeft from "../../assets/text-left.svg";
-import alingright from "../../assets/text-right.svg";
-import alingjustify from "../../assets/text-justify.svg";
-import alingcenter from "../../assets/text-center.svg";
-import lineCurve from "../../assets/line_curve.svg";
-import borderWidth from "../../assets/border_all.svg";
-import InputColor from "./InputColor";
-import { handleOptions } from "./TableGeneratorFN";
+import InputColor from './InputColor';
+import { handleOptions } from './TableGeneratorFN';
 
-import { TableInputs } from "./TableInputs";
+import { TableInputs } from './TableInputs';
+import Icon from '../Icon';
+import { ToolPane } from '../ToolsLayout/Sections';
 
 export const OptionsBoxTable = ({ tableConfig, setTableConfig }) => {
   return (
-    <div className="md:aspect-video  lg:px-9 mx-2 lg:w-1/2  text-sm flex flex-col gap-6 lg:justify-evenly font-bold ">
-      <div className="flex justify-between max-h-[36px]" id="Option1">
-        <span className="flex items-center">
-          <img
-            src={optionMenu}
-            alt="Option Menu"
-            className="w-[24px] h-[24px] m-1"
-          />
-          <h3 className="text-[32px] font-bold text-primary">Options</h3>
-        </span>
+    <ToolPane title="Options" icon="tune" isPrimary={true}>
+      <div className="flex flex-col gap-8 text-sm font-bold md:aspect-video lg:justify-evenly">
+        <section className="layout flex flex-wrap justify-between gap-y-4 [&>*]:w-[48%]">
+          <h3 className="border-b pb-1 border-[#D9D9D9] text-[18px] font-semibold min-w-full">
+            Layout
+          </h3>
 
-        <span className="flex items-center gap-4">
-          <img
-            src={addbookmark}
-            alt="book mark"
-            className="w-[24px] h-[24px]  cursor-pointer "
-          />
-          <img
-            alt="share Icon"
-            src={share}
-            className="w-[24px] h-[24px] cursor-pointer "
-          />
-        </span>
-      </div>
-
-      <div>
-        <h3 className="border-b font-semibold text-[18px] border-[#D9D9D9] ">
-          Layout
-        </h3>
-
-        <div id="Option2" className="flex flex-col md:flex-row gap-4">
           <TableInputs
             label="Rows"
-            iconSrc={rowsAndCols}
+            icon="table_rows"
             range={{ min: 2, max: 50 }}
             ShowUnits={false}
             tableConfig={tableConfig}
@@ -61,8 +27,7 @@ export const OptionsBoxTable = ({ tableConfig, setTableConfig }) => {
 
           <TableInputs
             label="Columns"
-            iconSrc={rowsAndCols}
-            rotate={true}
+            icon="view_week"
             range={{ min: 1, max: 50 }}
             ShowUnits={false}
             tableConfig={tableConfig}
@@ -70,143 +35,127 @@ export const OptionsBoxTable = ({ tableConfig, setTableConfig }) => {
             prop="dimensions"
             delay={true}
           />
-        </div>
-      </div>
+          <TableInputs
+            label="Vertical Cell Padding"
+            icon="padding"
+            range={{ min: 0, max: 100 }}
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+            prop="verticalCellPading"
+            delay={true}
+          />
 
-      <div id="Option3" className="flex flex-col md:flex-row gap-6">
-        <TableInputs
-          label="Vertical Cell Padding"
-          iconSrc={paddingIcons}
-          range={{ min: 0, max: 100 }}
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-          prop="verticalCellPading"
-          delay={true}
-        />
+          <TableInputs
+            label="Horizontal Cell Padding"
+            icon="padding"
+            range={{ min: 0, max: 100 }}
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+            prop="horizontalCellPading"
+          />
+          <TableInputs
+            label="Table Width"
+            icon="width"
+            range={{ min: 0, max: 1024 }}
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+            prop="tableWidth"
+          />
 
-        <TableInputs
-          label="Horizontal Cell Padding"
-          iconSrc={paddingIcons}
-          rotate={true}
-          range={{ min: 0, max: 100 }}
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-          prop="horizontalCellPading"
-        />
-      </div>
+          <label className="my-1 flex h-full max-h-[51px] w-full flex-col gap-1 text-sm font-semibold">
+            Text Alignment
+            <div className="flex gap-x-2  justify-around py-2 ">
+              <Icon
+                name="format_align_left"
+                alt="left"
+                onClick={e =>
+                  handleOptions(
+                    e,
+                    'textAlign',
+                    tableConfig,
+                    setTableConfig,
+                    e.target.alt
+                  )
+                }
+              />
+              <Icon
+                name="format_align_center"
+                alt="center"
+                onClick={e =>
+                  handleOptions(
+                    e,
+                    'textAlign',
+                    tableConfig,
+                    setTableConfig,
+                    e.target.alt
+                  )
+                }
+              />
+              <Icon
+                name="format_align_right"
+                alt="right"
+                onClick={e =>
+                  handleOptions(
+                    e,
+                    'textAlign',
+                    tableConfig,
+                    setTableConfig,
+                    e.target.alt
+                  )
+                }
+              />
+              <Icon
+                name="format_align_justify"
+                alt="justify"
+                onClick={e =>
+                  handleOptions(
+                    e,
+                    'textAlign',
+                    tableConfig,
+                    setTableConfig,
+                    e.target.alt
+                  )
+                }
+              />
+            </div>
+          </label>
+        </section>
 
-      <div id="Option4" className="flex flex-col md:flex-row gap-6">
-        <TableInputs
-          label="Table Width"
-          iconSrc={doubleArrows}
-          range={{ min: 0, max: 100 }}
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-          prop="tableWidth"
-        />
-
-        <label className="flex flex-col gap-1 font-semibold lg:mx-w-[242px] max-h-[51px] h-full w-full my-1 text-sm">
-          Text Alignment
-          <div className="flex gap-x-2  justify-around py-2 ">
-            <img
-              src={alingLeft}
-              className="w-4 h-4 cursor-pointer"
-              alt="start"
-              onClick={(e) =>
-                handleOptions(
-                  e,
-                  "textAlign",
-                  tableConfig,
-                  setTableConfig,
-                  e.target.alt
-                )
-              }
-            />
-            <img
-              src={alingcenter}
-              className="w-4 h-4 cursor-pointer"
-              alt="center"
-              onClick={(e) =>
-                handleOptions(
-                  e,
-                  "textAlign",
-                  tableConfig,
-                  setTableConfig,
-                  e.target.alt
-                )
-              }
-            />
-            <img
-              src={alingright}
-              className="w-4 h-4 cursor-pointer"
-              alt="end"
-              onClick={(e) =>
-                handleOptions(
-                  e,
-                  "textAlign",
-                  tableConfig,
-                  setTableConfig,
-                  e.target.alt
-                )
-              }
-            />
-            <img
-              src={alingjustify}
-              className="w-4 h-4 cursor-pointer"
-              alt="justify"
-              onClick={(e) =>
-                handleOptions(
-                  e,
-                  "textAlign",
-                  tableConfig,
-                  setTableConfig,
-                  e.target.alt
-                )
-              }
-            />
-          </div>
-        </label>
-      </div>
-
-      <h3 className="border-b font-semibold text-[18px] border-[#D9D9D9] ">
-        Borders
-      </h3>
-      <div id="Option5" className="grid grid-cols-1 grid-row-2  gap-6">
-        <TableInputs
-          label="Border Rounding"
-          iconSrc={lineCurve}
-          range={{ min: 0, max: 100 }}
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-          prop="borderRounding"
-        />
-
-        <TableInputs
-          label="Border Width"
-          iconSrc={borderWidth}
-          range={{ min: 0, max: 10 }}
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-          prop="borderWidth"
-        />
-
-        <div className="flex justify-between  ">
-          <div className="flex justify-start w-1/2 ">
-            <label className="flex flex-col gap-1 font-semibold lg:mx-w-[242px] max-h-[51px] h-full w-full my-1 text-sm max-w-24">
+        <section className="borders flex flex-wrap justify-between [&>*]:w-[48%] gap-y-4">
+          <h3 className="border-b border-[#D9D9D9] pb-1 text-[18px] font-semibold min-w-full">
+            Borders
+          </h3>
+          <TableInputs
+            label="Border Rounding"
+            icon="line_curve"
+            range={{ min: 0, max: 100 }}
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+            prop="borderRounding"
+          />
+          <TableInputs
+            label="Border Width"
+            icon="border_all"
+            range={{ min: 0, max: 10 }}
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+            prop="borderWidth"
+          />
+          <div className="flex justify-start items-center">
+            <label className="w-[10rem] text-sm font-semibold">
               Border Style:
             </label>
-
             <select
-              onChange={(e) =>
+              onChange={e =>
                 handleOptions(
                   e.target.value,
-                  "borderStyle",
+                  'borderStyle',
                   tableConfig,
                   setTableConfig
                 )
               }
               name=""
-              className="border border-gray-200 w-full max-w-60"
+              className="h-[43px] w-full rounded-[.25rem] border border-gray-200"
             >
               <option value="solid"> solid </option>
               <option value="none">none</option>
@@ -214,66 +163,64 @@ export const OptionsBoxTable = ({ tableConfig, setTableConfig }) => {
               <option value="dashed solid"> dashed solid </option>
               <option value="dashed double none"> dashed double none </option>
               <option value="dashed groove none dotted">
-                dashed groove none dotted{" "}
+                dashed groove none dotted{' '}
               </option>
               <option value="dashed groove none dotted">
-                dashed groove none dotted{" "}
+                dashed groove none dotted{' '}
               </option>
             </select>
           </div>
-
-          <div className="  flex gap-2 items-center justify-end w-1/2 ">
-            <strong className>Collapse</strong>
+          <div className="flex gap-2 items-center justify-end">
+            <strong className>Collapse Border</strong>
             <input
               type="checkbox"
-              onChange={(e) => {
+              onChange={e => {
                 handleOptions(
                   e.target.checked,
-                  "collapse",
+                  'collapse',
                   tableConfig,
                   setTableConfig
                 );
               }}
             />
           </div>
-        </div>
+        </section>
+        <section className="colors flex flex-wrap justify-between [&>*]:w-[48%] gap-y-4">
+          <h3 className="min-w-full border-b border-[#D9D9D9] pb-1 text-[18px] font-semibold">
+            Colors
+          </h3>
+          <InputColor
+            labelText="Text"
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+            prop="textColor"
+          />
+          <InputColor
+            labelText="Background"
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+            prop="bgColor"
+          />
+          <InputColor
+            labelText="Border"
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+            prop="borderColor"
+          />
+          <InputColor
+            labelText="Header Text"
+            prop="headerText"
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+          />
+          <InputColor
+            labelText="Header Background"
+            tableConfig={tableConfig}
+            setTableConfig={setTableConfig}
+            prop="headerBg"
+          />
+        </section>
       </div>
-
-      <h3 className="border-b font-semibold text-[18px] border-[#D9D9D9] ">
-        Colors
-      </h3>
-      <div id="Option5" className="grid md:grid-cols-2">
-        <InputColor
-          labelText="Text"
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-          prop="textColor"
-        />
-        <InputColor
-          labelText="Background"
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-          prop="bgColor"
-        />
-        <InputColor
-          labelText="Border"
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-          prop="borderColor"
-        />
-        <InputColor
-          labelText="Header Text"
-          prop="headerText"
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-        />
-        <InputColor
-          labelText="Header Background"
-          tableConfig={tableConfig}
-          setTableConfig={setTableConfig}
-          prop="headerBg"
-        />
-      </div>
-    </div>
+    </ToolPane>
   );
 };
--- fetools-app/src/pages/Table.jsx
+++ fetools-app/src/pages/Table.jsx
@@ -1,76 +1,71 @@
-import ToolHeading from "../components/ToolsLayout/ToolHeading";
-import ToolHeaderSection from "../components/ToolsLayout/ToolHeaderSection";
-import CodeBlock from "../components/CodeBlock";
-import PageSection from "../components/PageLayout/PageSection";
-import TabSwitcher from "../components/TabSwitcher";
-import GoDeeper from "../components/ToolsLayout/GoDeeper";
-import { OptionsBoxTable } from "../components/TableGeneratorComponents/OptionsBoxTable";
-import { TableGenerator } from "../components/TableGeneratorComponents/TableGenerator";
-import { useState } from "react";
-import "react-indiana-drag-scroll/dist/style.css";
-import { generateMultidimensionalArray } from "../components/TableGeneratorComponents/TableGeneratorFN";
-import { Scrollbars } from "react-custom-scrollbars-2";
+import ToolHeading from '../components/ToolsLayout/ToolHeading';
+import CodeBlock from '../components/CodeBlock';
+import TabSwitcher from '../components/TabSwitcher';
+import GoDeeper from '../components/ToolsLayout/GoDeeper';
+import { OptionsBoxTable } from '../components/TableGeneratorComponents/OptionsBoxTable';
+import { TableGenerator } from '../components/TableGeneratorComponents/TableGenerator';
+import { useState } from 'react';
+import useExpander from '../hooks/useExpander';
+import { generateMultidimensionalArray } from '../components/TableGeneratorComponents/TableGeneratorFN';
+import {
+  ToolPreviewPane,
+  ToolSection,
+  ToolSectionColumns,
+} from '../components/ToolsLayout/Sections';
+import ToolMain from '../components/ToolsLayout/ToolMain';
 
 const Table = () => {
   const [tableConfig, setTableConfig] = useState({
-    dimensions: generateMultidimensionalArray(24, 3),
-    verticalCellPading: 0,
+    dimensions: generateMultidimensionalArray(16, 3),
+    verticalCellPading: 4,
     horizontalCellPading: 0,
-    tableWidth: "500",
-    textAlign: "center",
+    tableWidth: '420',
+    textAlign: 'center',
     borderRounding: 25,
     borderWidth: 1,
-    borderStyle: "solid",
+    borderStyle: 'solid',
     collapse: true,
-    textColor: "#000",
-    bgColor: "#F1F1F1",
-    borderColor: "#000",
-    headerText: "#ffffff",
-    headerBg: "#FF007A",
+    textColor: '#000',
+    bgColor: '#F1F1F1',
+    borderColor: '#000',
+    headerText: '#ffffff',
+    headerBg: '#FF007A',
   });
+  const [isExpanded, toggleIsExpanded] = useExpander();
 
   return (
-    <>
-      <ToolHeaderSection>
-        <ToolHeading
-          title="Table Generator"
-          tagline="Output any number of rows and columns, with placeholder content and styling."
-        ></ToolHeading>
-      </ToolHeaderSection>
+    <ToolMain>
+      <ToolHeading
+        title="Table Generator"
+        tagline="Output any number of rows and columns, with placeholder content and styling."
+        icon="table"
+      ></ToolHeading>
 
-      <PageSection title="" icon="">
-        <div className="flex flex-col lg:flex-row-reverse lg:max-h-[786px] ">
-          <OptionsBoxTable
-            tableConfig={tableConfig}
-            setTableConfig={setTableConfig}
-          />
+      <ToolSectionColumns isExpanded={isExpanded}>
+        <OptionsBoxTable
+          tableConfig={tableConfig}
+          setTableConfig={setTableConfig}
+        />
 
-          <div className="display flex flex-col justify-center h-fit  m-7 lg:m-0   lg:w-1/2 w-full ">
-            <Scrollbars
-              style={{
-                width: "100%",
-                height: 608,
-                borderRadius: tableConfig.borderRounding,
-                borderWidth: tableConfig.borderWidth,
-                borderStyle: tableConfig.borderStyle,
-                borderColor: tableConfig.borderColor
-              }}
-              className=" max-h-[608px] w-full border-black "
-            >
-              <TableGenerator tableConfig={tableConfig} setTableConfig={setTableConfig} />
-              
-            </Scrollbars>
-            <div className="flex justify-between py-2" > <span>Click on any cell’s contents to edit.</span>   </div>
+        <ToolPreviewPane toggleIsExpanded={toggleIsExpanded}>
+          <div className="px-8 py-12 h-[40rem]">
+            <div className="w-full h-full max-h-full overflow-scroll flex items-start justify-start">
+              <TableGenerator
+                tableConfig={tableConfig}
+                setTableConfig={setTableConfig}
+              />
+            </div>
           </div>
-        </div>
-      </PageSection>
-      <PageSection
+          <p className="text-center">Click on any cell’s contents to edit.</p>
+        </ToolPreviewPane>
+      </ToolSectionColumns>
+      <ToolSection
         className=""
         title="Code Snippets"
         icon="integration_instructions"
       >
-        <TabSwitcher title="" buttons={["css", "tailwind"]}>
-          <CodeBlock title="" code={""} lang="css" />
+        <TabSwitcher title="" buttons={['css', 'tailwind']}>
+          <CodeBlock title="" code={''} lang="css" />
           <CodeBlock
             title=""
             code={` /* Add this to your tailwind.config.js file */
@@ -87,30 +82,28 @@ const Table = () => {
             lang="css"
           />
         </TabSwitcher>
-      </PageSection>
+      </ToolSection>
 
-      <PageSection title="Go Deeper" icon="school">
-        <GoDeeper
-          linksData={[
-            {
-              url: "https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow",
-              textValue:
-                "box-shadow from MDN - https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow ",
-            },
-            {
-              url: "https://www.w3schools.com/cssref/css3_pr_box-shadow.php",
-              textValue:
-                "box-shadow from W3 School - https://www.w3schools.com/cssref/css3_pr_box-shadow.php ",
-            },
-            {
-              url: "https://css-tricks.com/almanac/properties/b/box-shadow/",
-              textValue:
-                "box-shadow from Css-tricks - https://www.w3schools.com/cssref/css3_pr_box-shadow.php ",
-            },
-          ]}
-        />
-      </PageSection>
-    </>
+      <GoDeeper
+        linksData={[
+          {
+            url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow',
+            textValue:
+              'box-shadow from MDN - https://developer.mozilla.org/en-US/docs/Web/CSS/box-shadow ',
+          },
+          {
+            url: 'https://www.w3schools.com/cssref/css3_pr_box-shadow.php',
+            textValue:
+              'box-shadow from W3 School - https://www.w3schools.com/cssref/css3_pr_box-shadow.php ',
+          },
+          {
+            url: 'https://css-tricks.com/almanac/properties/b/box-shadow/',
+            textValue:
+              'box-shadow from Css-tricks - https://www.w3schools.com/cssref/css3_pr_box-shadow.php ',
+          },
+        ]}
+      />
+    </ToolMain>
   );
 };
 
