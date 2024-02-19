985df0b90865bd74258c25d97c3c2aeeff21ea48
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2024-02-18 22:54:58 -0500

    Allows emojis to be searched with U+

--- fetools-app/src/components/CharacterFinderComponents/CharacterCard.jsx
+++ fetools-app/src/components/CharacterFinderComponents/CharacterCard.jsx
@@ -1,3 +1,5 @@
+
+
 const CharacterCard = ({
   char,
   name,
@@ -7,47 +9,47 @@ const CharacterCard = ({
   cssCode,
 }) => {
   return (
-    <div className="p-6">
-      <p className="py-4 text-6xl font-bold text-left border-b-4 border-black">
+    <div className='p-6'>
+      <p className='py-4 text-6xl font-bold text-left border-b-4 border-black'>
         {char}
       </p>
-      <div className="flex flex-wrap">
-        <div className="w-full">
-          <p className="pt-2 font-bold text-left uppercase text-s">{name}</p>
+      <div className='flex flex-wrap'>
+        <div className='w-full'>
+          <p className='pt-2 font-bold text-left uppercase text-s'>{name}</p>
         </div>
-        <div className="flex flex-wrap">
-          <div className="w-full lg:w-1/2">
-            <p className="pt-2 text-xs text-left uppercase">
+        <div className='flex flex-wrap'>
+          <div className='w-full lg:w-1/2'>
+            <p className='pt-2 text-xs text-left uppercase'>
               unicode:{' '}
-              <span className="block font-bold normal-case">
+              <span className='block font-bold normal-case'>
                 {unicode || ' -- '}
               </span>
             </p>
           </div>
 
-          <div className="w-full lg:w-1/2">
-            <p className="pt-2 text-xs text-left uppercase">
+          <div className='w-full lg:w-1/2'>
+            <p className='pt-2 text-xs text-left uppercase'>
               html code:{' '}
-              <span className="block font-bold normal-case break-all">
+              <span className='block font-bold normal-case break-all'>
                 {htmlcode || ' -- '}
               </span>
             </p>
           </div>
 
-          <div className="w-full lg:w-1/2">
-            <p className="pt-2 text-xs text-left uppercase">
+          <div className='w-full lg:w-1/2'>
+            <p className='pt-2 text-xs text-left uppercase'>
               html entity:
-              <span className="block font-bold normal-case">
+              <span className='block font-bold normal-case'>
                 {htmlEntity !== undefined && htmlEntity !== ''
                   ? htmlEntity
                   : ' -- '}
               </span>
             </p>
           </div>
-          <div className="w-full lg:w-1/2">
-            <p className="pt-2 text-xs text-left uppercase">
+          <div className='w-full lg:w-1/2'>
+            <p className='pt-2 text-xs text-left uppercase'>
               css code:{' '}
-              <span className="block font-bold normal-case">
+              <span className='block font-bold normal-case'>
                 {cssCode || ' -- '}
               </span>
             </p>
--- fetools-app/src/components/SearchField.jsx
+++ fetools-app/src/components/SearchField.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 const SearchField = ({ placeholderText, search, clearInput }) => {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -10,21 +10,21 @@ const SearchField = ({ placeholderText, search, clearInput }) => {
 
   useEffect(() => {
     if (clearInput) {
-      setInputValue("");
-      search("");
+      setInputValue('');
+      search('');
     }
   }, [clearInput]);
 
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
+    if (e.key === 'Enter') {
       search(inputValue);
     }
     if (
-      e.key === "ArrowLeft" ||
-      e.key === "ArrowRight" ||
-      e.key === "ArrowUp" ||
-      e.key === "ArrowDown" ||
-      e.key === "Backspace"
+      e.key === 'ArrowLeft' ||
+      e.key === 'ArrowRight' ||
+      e.key === 'ArrowUp' ||
+      e.key === 'ArrowDown' ||
+      e.key === 'Backspace'
     ) {
       return;
     }
@@ -32,29 +32,29 @@ const SearchField = ({ placeholderText, search, clearInput }) => {
   };
 
   return (
-    <div className="flex justify-end relative">
-      <label className="relative">
+    <div className='flex justify-end relative'>
+      <label className='relative'>
       <input
-        type="text"
+        type='text'
         placeholder={placeholderText}
         value={inputValue}
         onChange={handleInputChange}
         onKeyDown={handleKeyDown}
-        className="bg-gray-100 p-3 text-lg pr-12 focus:[#663399] focus:ring focus:ring-[#663399] focus:outline-none"
+        className='bg-gray-100 p-3 text-lg pr-12 focus:ring focus:ring-[#663399] focus:outline-none'
         autoFocus
       />
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
           strokeWidth={1.5}
-          stroke="currentColor"
-          className="absolute right-3 top-1/2 transform -translate-y-1/2 w-6 h-6 pointer-events-none"
+          stroke='currentColor'
+          className='absolute right-3 top-1/2 transform -translate-y-1/2 w-6 h-6 pointer-events-none'
         >
           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
           />
         </svg>
         </label>
