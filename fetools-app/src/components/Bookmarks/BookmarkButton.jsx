845e7e3476eab02c5d2daf97ad9f1b125e900cba
joekotvas <joseph.kotvas@gmail.com>
2024-02-23 14:59:11 -0500

    Refactor bookmarks feature as a custom hook

---
+++ fetools-app/src/components/Bookmarks/BookmarkButton.jsx
@@ -0,0 +1,18 @@
+import Icon from "../Icon";
+
+export default function NewBookmark({
+    className,
+    isBookmarked,
+    toggleBookmark,
+    toolState,
+}) {
+    console.log("NewBookmark", isBookmarked, toolState);
+    return (
+        <button
+            onClick={() => toggleBookmark(isBookmarked, toolState)}
+            className={`material-symbols-rounded text-2xl w-[1.625rem] cursor-pointer hover:text-[#7F40BF] select-none ${className}`}
+        >
+            <Icon name={isBookmarked ? "bookmark_remove" : "bookmark_add"} />
+        </button>
+    );
+}
---
+++ fetools-app/src/components/Bookmarks/index.js
@@ -0,0 +1,13 @@
+import BookmarkButton from "./BookmarkButton";
+import BookmarkCard from "./BookmarkCard";
+import BookmarkCollection from "./BookmarkCollection";
+import BookmarkHoverActions from "./BookmarkHoverActions";
+import RemoveBookmarkButton from "./RemoveBookmarkButton";
+
+export {
+    BookmarkButton,
+    BookmarkCard,
+    BookmarkCollection,
+    BookmarkHoverActions,
+    RemoveBookmarkButton,
+};
