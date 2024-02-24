c12680d73ac2bc6b98f1490d48d48bd6ba46b41b
joekotvas <joseph.kotvas@gmail.com>
2024-02-24 02:09:32 -0500

    #152 Add empty state to collections.

--- fetools-app/src/components/Bookmarks/BookmarkCollection.jsx
+++ fetools-app/src/components/Bookmarks/BookmarkCollection.jsx
@@ -14,36 +14,45 @@ export default function BookmarkCollection({
     return (
         <div className="bookmark-collection flex justify-end items-end flex-col">
             {bookmarks.length ? (
-                <button
-                    onClick={() => setEditMode(!editMode)}
-                    className="text-2xl"
-                >
-                    {editMode ? (
-                        <Icon name="check" />
-                    ) : (
-                        <Icon name="app_registration" />
-                    )}
-                </button>
-            ) : null}
-            <div className="bookmark-cards-wrapper flex flex-wrap gap-4 w-full justify-start">
-                {bookmarks.map((item, idx) => {
-                    if (!item) return;
-                    return (
-                        <BookmarkCard
-                            key={idx}
-                            idx={idx}
-                            style={formatBookmarkCardStyle(item)}
-                            removeBookmark={() =>
-                                removeBookmark(item, bookmarks)
-                            }
-                            editMode={editMode}
-                            hoverActions={formatHoverActions(item)}
-                        >
-                            {formatBookmarkCardContents(item)}
-                        </BookmarkCard>
-                    );
-                })}
-            </div>
+                <>
+                    <button
+                        onClick={() => setEditMode(!editMode)}
+                        className="text-2xl"
+                    >
+                        {editMode ? (
+                            <Icon name="check" />
+                        ) : (
+                            <Icon name="app_registration" />
+                        )}
+                    </button>
+                    <div className="bookmark-cards-wrapper flex flex-wrap gap-4 w-full justify-start">
+                        {bookmarks.map((item, idx) => {
+                            if (!item) return;
+                            return (
+                                <BookmarkCard
+                                    key={idx}
+                                    idx={idx}
+                                    style={formatBookmarkCardStyle(item)}
+                                    removeBookmark={() =>
+                                        removeBookmark(item, bookmarks)
+                                    }
+                                    editMode={editMode}
+                                    hoverActions={formatHoverActions(item)}
+                                >
+                                    {formatBookmarkCardContents(item)}
+                                </BookmarkCard>
+                            );
+                        })}
+                    </div>
+                </>
+            ) : (
+                <div className="flex justify-center w-full">
+                    <p className="text-base italic">
+                        Nothing to see here yet. Bookmark items above to start
+                        collecting!
+                    </p>
+                </div>
+            )}
         </div>
     );
 }
