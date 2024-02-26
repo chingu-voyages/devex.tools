bfb3831ffa1f05f1ccf56c690b65a9ebae57f093
joekotvas <joseph.kotvas@gmail.com>
2024-02-26 17:20:38 -0500

    Restore BookmarkCollection

--- fetools-app/src/components/ColorGradient/ColorGradientBookmarks.jsx
+++ fetools-app/src/components/ColorGradient/ColorGradientBookmarks.jsx
@@ -12,5 +12,86 @@ export default function ColorGradientBookmarks({
     setGradientColors,
     inputValue,
 }) {
-    return <ToolSection title="Your Collection" icon="bookmarks"></ToolSection>;
+    return (
+        <ToolSection title="Your Collection" icon="bookmarks">
+            <BookmarkCollection
+                bookmarks={bookmarks}
+                removeBookmark={removeBookmark}
+                formatBookmarkCardStyle={(item) => {
+                    console.log("item", item);
+                    if (!item) {
+                        return;
+                    }
+                    return {
+                        background: `
+                            ${item?.type}-gradient(
+                            ${
+                                item?.type === "radial"
+                                    ? ""
+                                    : `${item?.rotation}deg,`
+                            } 
+                            ${item?.gradientColors
+                                .map((color) => color?.colorStr)
+                                .join(", ")})`,
+                        padding: "1rem",
+                    };
+                }}
+                formatHoverActions={(item) => {
+                    if (!item) {
+                        return;
+                    }
+                    const newGradientCode = `
+                    ${item?.type}-gradient(
+                    ${item?.type === "radial" ? "" : `${item?.rotation}deg,`} 
+                    ${item?.gradientColors
+                        .map((color) => color?.colorStr)
+                        .join(", ")})`;
+                    const EyeDropAction = (
+                        <EyeDropButton
+                            key="EyeDropButton"
+                            title={"New Gradient Set"}
+                            setState={() => {
+                                setColorsArr(item.gradientColors);
+                                setInputValue({
+                                    ...inputValue,
+                                    rotation: item.rotation / 3.6,
+                                    type: item.type,
+                                });
+                                setGradientColors(
+                                    item.gradientColors.map(
+                                        (paramColor, idx) => ({
+                                            ...paramColor,
+                                            value:
+                                                idx ===
+                                                item.gradientColors.length - 1
+                                                    ? 100
+                                                    : (100 /
+                                                          (item.gradientColors
+                                                              .length -
+                                                              1)) *
+                                                      idx,
+                                        })
+                                    )
+                                );
+                            }}
+                            newValue={newGradientCode}
+                            toastState={toastState}
+                        />
+                    );
+                    const CopyAction = (
+                        <CopyButton
+                            key="CopyButton"
+                            onCopy={() => newGradientCode}
+                            toastState={toastState}
+                        />
+                    );
+                    return (
+                        <BookmarkHoverActions
+                            actions={[EyeDropAction, CopyAction]}
+                        ></BookmarkHoverActions>
+                    );
+                }}
+            />
+        </ToolSection>
+    );
 }
